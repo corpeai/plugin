40e9236be828a02f43fec320d5a12303f649bc5b
CorpAI <35112540+corpeai@users.noreply.github.com>
2025-12-03 14:49:58 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,6 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Script from "next/script";
 
-function getQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-        staleTime: 30000,
-        retry: false,
-      },
-    },
-  });
-}
-
 window.Jupiter.init({
   displayMode: "integrated",
   integratedTargetId: "target-container",
