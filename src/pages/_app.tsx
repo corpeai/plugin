74edb7b520efbb5f24f4e2e3b61cea5bbffe70cc
CorpAI <35112540+corpeai@users.noreply.github.com>
2025-12-04 12:23:12 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -235,15 +235,7 @@ export default function App() {
                             <UnifiedWalletButton />
                           </div>
                         </div>
-                        <PluginGroup tab={displayMode} />
-                      </div>
-                      <span className="flex justify-center text-center text-xs text-[#9D9DA6] mb-2">
-                        {displayMode === 'modal' ? 'Jupiter renders as a modal and takes up the whole screen.' : null}
-                        {displayMode === 'integrated' ? 'Jupiter renders as a part of your dApp.' : null}
-                        {displayMode === 'widget'
-                          ? 'Jupiter renders as part of a widget that can be placed at different positions on your dApp.'
-                          : null}
-                      </span>
+                        <JupiterTerminal mode="integrated" onClose={() => onOpenChange(false)} />
                     </div>
                   </ShouldWrapWalletProvider>
                 </div>
