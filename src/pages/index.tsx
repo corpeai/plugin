4d247b17d52fc99523f861a462fa7e1d8726704d
CorpAI <35112540+corpeai@users.noreply.github.com>
2025-12-03 20:53:15 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PluginComponent from 'src/components/plugin';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <PluginComponent />
