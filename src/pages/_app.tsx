c12eb016a4be8e60554a6dccfe6773913474d106
CorpAI <35112540+corpeai@users.noreply.github.com>
2025-12-06 01:52:59 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -51,15 +51,23 @@ const queryClient = new QueryClient({
 
 const PLUGIN_MODE: { label: string; value: IInit['displayMode'] }[] = [
   {
+    label: 'Modal',
+    value: 'modal',
+  },
+  {
     label: 'Integrated',
     value: 'integrated',
+  },
+  {
+    label: 'Widget',
+    value: 'widget',
   },
 ];
 
 export default function App() {
   const [displayMode, setDisplayMode] = useState<IInit['displayMode']>('integrated');
   const [isSideDrawerOpen, setIsSideDrawerOpen] = useState(false);
-  const [sideDrawerTab, setSideDrawerTab] = useState(false);
+  const [sideDrawerTab, setSideDrawerTab] = useState<'config' | 'snippet'>('config');
 
   // Cleanup on tab change
   useEffect(() => {
@@ -187,16 +195,15 @@ export default function App() {
 
               <div className="flex justify-center">
                 <div className="max-w-[420px] mt-8 rounded-3xl flex flex-col md:flex-row w-full relative border border-white/10">
-                 </div>
-                  <ShouldWrapWalletProvider />
-                 </div>
-
+                  <ShouldWrapWalletProvider>
+                    <div className=" h-full w-full rounded-xl flex flex-col">
+                    </div>
                       <div className="flex flex-grow  justify-center text-white/75 flex-col mx-auto px-2 ">
                         <div className="flex flex-row justify-between  min-h-[57px] items-center">
                           <div className="flex justify-between flex-row">
                             <button
                               className="relative text-sm text-landing-primary font-semibold p-1  rounded-lg underline"
-                              onClick={() => setIsSideDrawerOpen(false)}
+                              onClick={() => setIsSideDrawerOpen(true)}
                             >
                               Customize
                               {/* <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 256 256"><path fill="currentColor" d="M128 80a48 48 0 1 0 48 48a48.05 48.05 0 0 0-48-48m0 80a32 32 0 1 1 32-32a32 32 0 0 1-32 32m88-29.84q.06-2.16 0-4.32l14.92-18.64a8 8 0 0 0 1.48-7.06a107.2 107.2 0 0 0-10.88-26.25a8 8 0 0 0-6-3.93l-23.72-2.64q-1.48-1.56-3-3L186 40.54a8 8 0 0 0-3.94-6a107.7 107.7 0 0 0-26.25-10.87a8 8 0 0 0-7.06 1.49L130.16 40h-4.32L107.2 25.11a8 8 0 0 0-7.06-1.48a107.6 107.6 0 0 0-26.25 10.88a8 8 0 0 0-3.93 6l-2.64 23.76q-1.56 1.49-3 3L40.54 70a8 8 0 0 0-6 3.94a107.7 107.7 0 0 0-10.87 26.25a8 8 0 0 0 1.49 7.06L40 125.84v4.32L25.11 148.8a8 8 0 0 0-1.48 7.06a107.2 107.2 0 0 0 10.88 26.25a8 8 0 0 0 6 3.93l23.72 2.64q1.49 1.56 3 3L70 215.46a8 8 0 0 0 3.94 6a107.7 107.7 0 0 0 26.25 10.87a8 8 0 0 0 7.06-1.49L125.84 216q2.16.06 4.32 0l18.64 14.92a8 8 0 0 0 7.06 1.48a107.2 107.2 0 0 0 26.25-10.88a8 8 0 0 0 3.93-6l2.64-23.72q1.56-1.48 3-3l23.78-2.8a8 8 0 0 0 6-3.94a107.7 107.7 0 0 0 10.87-26.25a8 8 0 0 0-1.49-7.06Zm-16.1-6.5a74 74 0 0 1 0 8.68a8 8 0 0 0 1.74 5.48l14.19 17.73a91.6 91.6 0 0 1-6.23 15l-22.6 2.56a8 8 0 0 0-5.1 2.64a74 74 0 0 1-6.14 6.14a8 8 0 0 0-2.64 5.1l-2.51 22.58a91.3 91.3 0 0 1-15 6.23l-17.74-14.19a8 8 0 0 0-5-1.75h-.48a74 74 0 0 1-8.68 0a8 8 0 0 0-5.48 1.74l-17.78 14.2a91.6 91.6 0 0 1-15-6.23L82.89 187a8 8 0 0 0-2.64-5.1a74 74 0 0 1-6.14-6.14a8 8 0 0 0-5.1-2.64l-22.58-2.52a91.3 91.3 0 0 1-6.23-15l14.19-17.74a8 8 0 0 0 1.74-5.48a74 74 0 0 1 0-8.68a8 8 0 0 0-1.74-5.48L40.2 100.45a91.6 91.6 0 0 1 6.23-15L69 82.89a8 8 0 0 0 5.1-2.64a74 74 0 0 1 6.14-6.14A8 8 0 0 0 82.89 69l2.51-22.57a91.3 91.3 0 0 1 15-6.23l17.74 14.19a8 8 0 0 0 5.48 1.74a74 74 0 0 1 8.68 0a8 8 0 0 0 5.48-1.74l17.77-14.19a91.6 91.6 0 0 1 15 6.23L173.11 69a8 8 0 0 0 2.64 5.1a74 74 0 0 1 6.14 6.14a8 8 0 0 0 5.1 2.64l22.58 2.51a91.3 91.3 0 0 1 6.23 15l-14.19 17.74a8 8 0 0 0-1.74 5.53Z"/></svg> */}
@@ -214,10 +221,14 @@ export default function App() {
                         <PluginGroup tab={displayMode} />
                       </div>
                       <span className="flex justify-center text-center text-xs text-[#9D9DA6] mb-2">
+                        {displayMode === 'modal' ? 'Jupiter renders as a modal and takes up the whole screen.' : null}
                         {displayMode === 'integrated' ? 'Jupiter renders as a part of your dApp.' : null}
+                        {displayMode === 'widget'
+                          ? 'Jupiter renders as part of a widget that can be placed at different positions on your dApp.'
+                          : null}
                       </span>
                     </div>
-                  <ShouldWrapWalletProvider />
+                  </ShouldWrapWalletProvider>
                 </div>
               </div>
             </div>
@@ -225,7 +236,7 @@ export default function App() {
           <Upsell/>
           <Footer />
         </div>
-      <FormProvider />
-    <QueryClientProvider />
+      </FormProvider>
+    </QueryClientProvider>
   );
 }
