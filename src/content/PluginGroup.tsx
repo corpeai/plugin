b32d6db6ef2214db50f513debf0d145e61af0ed8
CorpAI <35112540+corpeai@users.noreply.github.com>
2025-12-06 15:12:43 -0500

    Update PluginGroup.tsx

--- src/content/PluginGroup.tsx
+++ src/content/PluginGroup.tsx
@@ -9,7 +9,7 @@ interface PluginGroupProps {
 
 export const PluginGroup = ({ tab }: PluginGroupProps) => {
   return (
-    <div className='min-h-[550px] w-[360px]'>
+    <div className='min-h-[550px] w-[4200px]'>
       {tab === 'modal' ? <ModalPlugin /> : null}
       {tab === 'integrated' ? <IntegratedPlugin /> : null}
       {tab === 'widget' ? <WidgetPlugin /> : null}
